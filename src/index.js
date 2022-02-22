b0ff80d8a6494177e9f2be70733f0bd9befd1fbb
VasilinaFreedom <95901200+VasilinaFreedom@users.noreply.github.com>
2022-02-22 22:49:05 +0300

    Add my function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
+    let arr = Array.from(String(n), Number)
+    let rev = arr.reverse()
+    rev.length = 3
+    let res = rev.join('')
+    return res
   
 }
